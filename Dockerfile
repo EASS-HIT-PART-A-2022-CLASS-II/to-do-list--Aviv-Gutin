b2f91eeb7d05faab2ade4f08cbc8a466f3e6cfed
avivgutin <avivgutin@gmail.com>
2023-02-10 19:30:05 +0200

    Update

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,4 @@ FROM python:3.9
 WORKDIR /app
 COPY . .
 RUN pip install -r requirements.txt
+RUN pip install redis
--- main.py
+++ main.py
@@ -1,44 +1,39 @@
 import json
+import redis
 from fastapi import FastAPI
+from redisjson import Client
 
+
+r = redis.Redis()
+redis_json = Client(r)
 app = FastAPI()
-
-
-def get_tasks():
-    with open("tasks.json", "r") as f:
-        tasks = json.load(f)
-    return tasks
-
-
-def save_tasks(tasks):
-    with open("tasks.json", "w") as f:
-        json.dump(tasks, f)
+TASKS_KEY = "tasks"
 
 
 @app.get("/tasks")
 def read_tasks():
-    return get_tasks()
+   return redis_json.jsonget(TASKS_KEY, [])
 
 
 @app.post("/tasks")
 def create_task(task: str):
-    tasks = get_tasks()
-    tasks.append({"task": task})
-    save_tasks(tasks)
-    return {"task": task}
+   tasks = redis_json.jsonget(TASKS_KEY, [])
+   tasks.append({"task": task})
+   redis_json.jsonset(TASKS_KEY, tasks)
+   return {"task": task}
 
 
 @app.put("/tasks/{task_id}")
 def update_task(task_id: int, task: str):
-    tasks = get_tasks()
-    tasks[task_id] = {"task": task}
-    save_tasks(tasks)
-    return {"task_id": task_id, "task": task}
+   tasks = redis_json.jsonget(TASKS_KEY, [])
+   tasks[task_id] = {"task": task}
+   redis_json.jsonset(TASKS_KEY, tasks)
+   return {"task_id": task_id, "task": task}
 
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    tasks = get_tasks()
-    tasks.pop(task_id)
-    save_tasks(tasks)
-    return {"task_id": task_id}
+   tasks = redis_json.jsonget(TASKS_KEY, [])
+   tasks.pop(task_id)
+   redis_json.jsonset(TASKS_KEY, tasks)
+   return {"task_id": task_id}
